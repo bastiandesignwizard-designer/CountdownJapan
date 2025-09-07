01f234bd26c968095c50fe2f4dde3cd0068de07b
bastiandesignwizard-designer <bastiandesignwizard@gmail.com>
2025-09-07 20:19:00 +0200

    Update and rename script.js to countDown.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function handleTickInit(tick) {
-  var nextYear = (new Date()).getFullYear() + 1;
-  
-  Tick.count.down(nextYear + '-01-01').onupdate = function(value) {
-    tick.value = value;
-  };
-}
