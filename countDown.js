c44783386e09a7ee08c9ffb69b942f001952c659
bastiandesignwizard-designer <bastiandesignwizard@gmail.com>
2025-09-07 20:22:41 +0200

    Update countDown.js

--- countDown.js
+++ countDown.js
@@ -343,3 +343,19 @@ if (typeof(window) !== 'undefined') {
     window.Countdown = Countdown;
 }
 
+var cd = new Countdown({
+    cont: document.querySelector('.container'),
+    endDate: 1553468400000,
+    outputTranslation: {
+        year: 'Years',
+        week: 'Weeks',
+        day: 'Days',
+        hour: 'Hours',
+        minute: 'Minutes',
+        second: 'Seconds',
+    },
+    endCallback: null,
+    outputFormat: 'week|day|hour|minute|second',
+});
+
+
