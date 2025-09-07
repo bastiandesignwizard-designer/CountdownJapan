6c1c98bba98648ef31e83096b34493ff8ae6149a
bastiandesignwizard-designer <bastiandesignwizard@gmail.com>
2025-09-07 20:53:58 +0200

    Update countDown.js

--- countDown.js
+++ countDown.js
@@ -1,4 +1,4 @@
-export default class Countdown {
+class Countdown {
 
     get TIMESTAMP_SECOND() { return 1000; }
     get TIMESTAMP_MINUTE() { return 60 * this.TIMESTAMP_SECOND; }
@@ -357,6 +357,7 @@ var cd = new Countdown({
     endCallback: null,
     outputFormat: 'week|day|hour|minute|second',
 });
+
 
 
 
